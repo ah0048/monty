177f55b4cdc1f98a067b2c2f420ec2dda71c3e49
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-03-28 18:25:50 +0200

    edit

--- pchar.c
+++ pchar.c
@@ -7,18 +7,18 @@
  */
 void pchar(stack_t **stack, unsigned int line_number)
 {
-    stack_t *first = *stack;
+	stack_t *first = *stack;
 
-    if (*stack == NULL)
-    {
-        fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    if (first->n < 32 || first->n > 127)
-    {
-        fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    putchar(first->n);
-    putchar('\n');
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (first->n < 32 || first->n > 127)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	putchar(first->n);
+	putchar('\n');
 }
